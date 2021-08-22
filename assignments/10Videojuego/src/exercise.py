3171d59452a7649ae4ea46438e16228e44115c8d
AnaLauraCollantes <a01641150@itesm.mx>
2021-08-22 02:10:46 +0000

    Tarea1_enviofinal

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    juegos_n = int(input('Dame la cantidad de juegos nuevos: '))
+    juegos_u = int(input('Dame la cantidad de juegos usados: '))
+    total = (juegos_n*1000)+(juegos_u*350)
+    print('El total de la compra es: '+str(total))
+    
 
 
 
